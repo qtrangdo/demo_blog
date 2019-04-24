b386813d83ff7c28d3343a02e9f5e038f722b101
td94 <quynhtrangdo94@gmail.com>
2019-04-24 19:51:28 -0400

    Implement create new comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(param[:post_id])
-    @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
-    redirect_to post_path(@post)
-  end
+		@post = Post.find(params[:post_id])
+	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
+		redirect_to post_path(@post)	
+	end
 
   def destroy
   end
