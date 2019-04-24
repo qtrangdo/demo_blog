85f8a374b7ff24a56cb592c571188f79de26e4c8
td94 <quynhtrangdo94@gmail.com>
2019-04-24 19:18:05 -0400

    Count and display total number of comments on a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(param[:post_id])
+    @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+  end
+
 end
