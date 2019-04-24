5ed2f0097e211dd6d6f578eb00848a2aadf5ca6f
td94 <quynhtrangdo94@gmail.com>
2019-04-24 19:10:47 -0400

    Establish relationship between post and comment models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_230106) do
+ActiveRecord::Schema.define(version: 2019_04_24_230813) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
